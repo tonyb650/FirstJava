45209138db5b5cca33aab614355a666fad90558f
Anthony Brierly <anthonylbrierly@gmail.com>
2023-10-24 07:02:55 -0700

    First Commit

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My World");
+        System.out.println("Again World");
+        System.out.println("Hello World");
+    }
+}
